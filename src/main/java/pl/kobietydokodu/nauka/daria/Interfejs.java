ebc441697134d143844480e5aa7e19f3269f114c
Daria <DARIAPIERZGALSKA@gmail.com>
2019-04-08 23:23:34 +0200

    Dodano zmiany w klasie interfejs - zadanie z lekcji #2 interakcja z użytkownikiem

--- src/main/java/pl/kobietydokodu/nauka/daria/Interfejs.java
+++ src/main/java/pl/kobietydokodu/nauka/daria/Interfejs.java
@@ -1,5 +1,21 @@
 package pl.kobietydokodu.nauka.daria;
 
+import java.util.Scanner;
+
 public class Interfejs {
+    static Scanner scanner = new Scanner(System.in);
+
+    public static void main(String[] args) {
+        Kot kot = new Kot();
+        System.out.println("Podaj imię kota:");
+        kot.setImie(userInput());
+        System.out.println("Podaj imię opiekuna kota:");
+        kot.setImieOpiekuna(userInput());
+        System.out.println("Dziękuję za podanie inforacji o kocie o imieniu " +kot.getImie()+" i jego opiekunie o imieniu "+ kot.getImieOpiekuna()+".");
+    }
+
+    static String userInput() {
+        return scanner.nextLine();
+    }
 
 }
